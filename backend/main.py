724836e648a9f4ab85e903577d39730f509a68d6
findingaadi <adarshaaryal30@gmail.com>
2025-02-11 13:29:40 +0000

    end point to add users

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,9 @@
-from fastapi import FastAPI
-from database import engine
-from models import Base
+from fastapi import FastAPI, Depends
+from database import engine, SessionLocal
+from models import Base,User, PatientRecord
+from pydantic import BaseModel
+from fastapi import HTTPException
+from sqlalchemy.orm import Session
 
 app = FastAPI()
 
@@ -10,3 +13,38 @@ Base.metadata.create_all(bind=engine)
 @app.get("/")
 def home():
     return {"message":"welcome!"}
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+#schema to create a new user
+class UserCreate(BaseModel):
+    name:str
+    email:str
+    password:str
+    role:str
+
+
+@app.post("/users/")
+def create_user(user: UserCreate,db: Session =Depends(get_db)):
+    existing_user = db.query(User).filter(User.email == user.email).first()
+    if existing_user:
+        raise HTTPException(status_code=400, details ="email already taken by another user")
+    
+    new_user = User(
+        name = user.name,
+        email = user.email,
+        password = user.password,
+        role = user.role 
+    )
+
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
