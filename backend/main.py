3bc9f278d48ee895845b5c0f8265fff74c0fe971
findingaadi <adarshaaryal30@gmail.com>
2025-02-11 19:46:17 +0000

    users management API complete and functional

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, Body
 from database import engine, SessionLocal
 from models import Base,User, PatientRecord
 from pydantic import BaseModel, EmailStr
@@ -51,6 +51,83 @@ def create_user(user: UserCreate,db: Session =Depends(get_db)):
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
-    
+
     return new_user
 
+class UserResponse(BaseModel):
+    id: int
+    name: str
+    email: str
+    role: str
+
+    # To receive the response as in JSON format
+    class config: 
+        orm_model = True
+
+@app.get("/users/", response_model= list[UserResponse])
+def get_users(db: Session = Depends(get_db)):
+    users = db.query(User).all()
+    return users
+
+
+# To view a specific user based on their userID
+@app.get("/users/{user_id}", response_model= UserResponse)
+def get_user(user_id: int, db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.id == user_id).first()
+    if not user:
+        raise HTTPException(status_code=404, detail= "user is not found, please try again.")
+    return user
+
+
+# To update a users details
+
+class UserUpdate(BaseModel):
+    name: str | None = None # this leaves the ones that are not updated as it is
+    email: EmailStr | None = None 
+    role : str | None = None
+    
+    class config: 
+        orm_model = True
+
+
+"""
+there was an error with the post endpoint to update a user,
+the fastapi was misinterpreting the user_update as a query parameter,
+added the body(...) forcing the fastapi to read the request as a body of JSON, 
+can declare the userid and then update its attributes on uvicorn. 
+"""
+
+@app.put("/users/{user_id}", response_model = UserResponse)
+def update_user(user_id: int, user_update: UserUpdate = Body(...) , db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.id == user_id).first()
+    if not user:
+        raise HTTPException(status_code= 404, detail= "user not found, please try again.")
+    
+    if user_update.name:
+        user.name = user_update.name
+
+    if user_update.email:
+        # check if the provided email is already existing or not
+        existing_user = db.query(User).filter(User.email == user_update.email).first()
+        if existing_user:
+            raise HTTPException(status_code=400, detail="email is already in use.")
+        # if not then update the email
+        user.email = user_update.email
+
+    if user_update.role:
+        user.role = user_update.role
+    
+    db.commit()
+    db.refresh(user)
+    return user
+
+
+@app.delete("/users/{user_id}")
+def delete_user(user_id: int, db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.id == user_id).first()
+    if not user:
+        raise HTTPException(status_code= 400, detail="user not found, please try again.")
+    
+    db.delete(user)
+    db.commit()
+    return{user.name,"has been deleted off the record."}
