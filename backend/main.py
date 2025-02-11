739b6e9328ea5b6900030e2d4be6c03057eb1aa1
findingaadi <adarshaaryal30@gmail.com>
2025-02-11 15:28:10 +0000

    implemented the recommeneded changes from code review

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI, Depends
 from database import engine, SessionLocal
 from models import Base,User, PatientRecord
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 from fastapi import HTTPException
 from sqlalchemy.orm import Session
-
+from passlib.hash import pbkdf2_sha256
 app = FastAPI()
 
 #create database tables
@@ -25,10 +25,13 @@ def get_db():
 #schema to create a new user
 class UserCreate(BaseModel):
     name:str
-    email:str
+    email:EmailStr
     password:str
     role:str
 
+
+def hash_password(password:str):
+    return pbkdf2_sha256.hash(password)
 
 @app.post("/users/")
 def create_user(user: UserCreate,db: Session =Depends(get_db)):
@@ -36,15 +39,18 @@ def create_user(user: UserCreate,db: Session =Depends(get_db)):
     if existing_user:
         raise HTTPException(status_code=400, details ="email already taken by another user")
     
+    hashed_password = hash_password(user.password)
+
     new_user = User(
         name = user.name,
         email = user.email,
-        password = user.password,
+        password = hashed_password,
         role = user.role 
     )
 
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
+    
     return new_user
 
